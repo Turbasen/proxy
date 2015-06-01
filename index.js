d9170bb469557979cc1b9a84586a2eea731cb63f
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-06-01 21:49:54 +0200

    Tag release v1.0.0

---
+++ index.js
@@ -0,0 +1,55 @@
+var url = require('url');
+var http = require('http');
+var httpProxy = require('http-proxy');
+
+var port = process.env.VIRTUAL_PORT || 8080;
+var api = {
+  env: process.env.NTB_ENV || 'api',
+  key: process.env.NTB_KEY
+};
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // Add CORS Headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, OPTIONS');
+  res.setHeader('Access-Control-Expose-Headers', [
+    'Count-Return',
+    'Count-Total',
+    'ETag',
+    'Last-Modified',
+    'Location',
+    'X-Cache-Hit',
+    'X-Ratelimit-Limit',
+    'X-Ratelimit-Remaining',
+    'X-Ratelimit-Reset'
+  ].join(' '));
+
+  // COORS preflight
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+  }
+
+  // Add HTTP Header
+  req.headers.host = api.env + '.nasjonalturbase.no'
+
+  // Add API key parameter
+  u = url.parse(req.url, true);
+  u.query.api_key = api.key;
+  req.url = url.format(u);
+
+  proxy.web(req, res, { target: 'http://' + req.headers.host });
+});
+
+console.log('listening on port ' + port)
+server.listen(port);
